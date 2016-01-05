c6e8270ba6b85c5c9773db90da5a7641f788722f
Navid Farhadi <navid@navidfarhadi.com>
2016-01-05 15:08:59 -0800

    Implemented basic interface and added AminoAcidGenerator class

---
+++ src/AminoAcidGenerator.java
@@ -0,0 +1,14 @@
+/**
+ * Created by navid on 1/5/16.
+ */
+public class AminoAcidGenerator
+{
+    public String DNASequence;
+
+    public AminoAcidGenerator(String inputDNASequence)
+    {
+        DNASequence = inputDNASequence;
+    }
+}
+
+
--- src/GeneFinder.java
+++ src/GeneFinder.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created by navid on 1/4/16.
  */
@@ -5,6 +7,34 @@ public class GeneFinder
 {
     public static void main(String[] args)
     {
-        System.out.println("GeneFinder: Initial Commit");
+        String inputDNASequence;
+        Scanner scanInputDNASequence = new Scanner(System.in);
+
+        System.out.print("Please enter a valid DNA Sequence from 5' to 3': ");
+        inputDNASequence = scanInputDNASequence.next();
+
+        while(!isDNASequenceValid(inputDNASequence))
+        {
+            System.out.println("Invalid sequence. Note that a valid DNA Sequence consists of only nucleotides A,C,G,T and must be at least 3 nucleotides long.");
+            System.out.print("Please enter a valid DNA Sequence: ");
+            inputDNASequence = scanInputDNASequence.next();
+        }
+
+        AminoAcidGenerator aminoAcidGenerator = new AminoAcidGenerator(inputDNASequence);
+    }
+
+    public static boolean isDNASequenceValid(String DNASequence)
+    {
+        if(DNASequence == null || DNASequence.isEmpty() || (DNASequence.length() < 3))
+            return false;
+
+        for(int i = 0 ; i < DNASequence.length() ; i++)
+        {
+            char currentChar = DNASequence.charAt(i);
+            if(!"ACTG".contains(Character.toString(currentChar)))
+                return false;
+        }
+
+        return true;
     }
 }
